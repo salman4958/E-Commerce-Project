6b67241fbf50559e7cdafdd123c2c343110df8c7
Mohammed Salman <62874800+salman4958@users.noreply.github.com>
2023-04-12 12:02:07 +0530

    Update LoginServlet.java

--- com.shopNest/src/main/java/com/shopNest/customer/LoginServlet.java
+++ com.shopNest/src/main/java/com/shopNest/customer/LoginServlet.java
@@ -8,18 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/log")
-public class LoginServlet  extends HttpServlet{
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+public class LoginServlet  extends HttpServlet
+{
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+	{
 		String uname=req.getParameter("uname");
 		String pass=req.getParameter("pass");
 		boolean val=Validator.isValid(uname,pass);
-		if(val==true && uname.equals("admin")) {
+		if(val==true && uname.equals("admin")) 
+		{
 			resp.sendRedirect("admin.jsp");
 		}
-		else if(val==true) {
+		else if(val==true) 
+		{
 			resp.sendRedirect("home.jsp");
 		}
-		else {
+		else 
+		{
 			resp.sendRedirect("login.jsp");
 		}
 	}
